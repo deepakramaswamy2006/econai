c803dda8b4b3e3a89c7ffe45f4b15374c238728c
deepak <deepakramaswamy13@gmail.com>
2026-05-08 00:58:58 +0530

    Fix CORS and production origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5175", "http://127.0.0.1:5175", "http://localhost:5176", "http://127.0.0.1:5176"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
